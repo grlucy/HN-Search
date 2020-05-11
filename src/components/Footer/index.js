ec27b742d899d28a670764140495258f7a7274fb
unknown <53538316+grlucy@users.noreply.github.com>
2020-05-11 18:31:34 -0400

    styled header and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
+    <footer className="bg-light">
       <div className="container">
         <div className="row">
           <div className="col">
-            <p>&copy; Gina Lucy 2020</p>
+            <p className="text-muted text-center mt-2">&copy; Gina Lucy 2020</p>
           </div>
         </div>
       </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="bg-dark mb-4">
       <div className="container">
         <div className="row">
-          <div className="col">This is Header</div>
+          <div className="col">
+            <h1 className="text-white text-center py-2">Search Hacker News</h1>
+          </div>
         </div>
       </div>
     </header>
