74fd692da553d02b6cff5d5e83e899fd265fcf84
unknown <53538316+grlucy@users.noreply.github.com>
2020-05-11 18:45:34 -0400

    styled Sort component

--- src/components/Sort/index.js
+++ src/components/Sort/index.js
@@ -3,7 +3,21 @@ import React from "react";
 function Sort() {
   return (
     <>
-      <p>Sort results by:</p>
+      <form>
+        <div className="d-sm-flex align-items-center">
+          <label className="mr-3 mb-0 font-weight-bold">Sort results by:</label>
+          <select
+            className="form-control form-control-sm"
+            style={{ maxWidth: "150px" }}
+          >
+            <option value="Relevance">Relevance</option>
+            <option value="Date">Date</option>
+          </select>
+          <button type="submit" className="btn btn-sm btn-primary ml-1">
+            Sort
+          </button>
+        </div>
+      </form>
     </>
   );
 }
