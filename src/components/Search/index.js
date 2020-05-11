1c04b725f5cab822ca5a3a02fdbb4de690cb5249
unknown <53538316+grlucy@users.noreply.github.com>
2020-05-11 19:30:26 -0400

    styled Search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function Search() {
   return (
-    <div className="searchHeader bg-primary">
-      <h3 className="text-white">Search</h3>
+    <div className="searchHeader bg-primary rounded p-3 mb-3">
+      <div className="d-flex align-items-center justify-content-between">
+        <h4 className="text-white mb-0">Search</h4>
+        <h4 className="text-white mb-0">
+          <i className="fas fa-chevron-up"></i>
+        </h4>
+      </div>
     </div>
   );
 }
