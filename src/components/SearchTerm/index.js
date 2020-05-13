02da11e43bbd1cfc87b5629bceae90855ba02f15
unknown <53538316+grlucy@users.noreply.github.com>
2020-05-13 14:26:31 -0400

    added onClick actions to SearchTerm and form validation to Searchbar

--- src/components/SearchTerm/index.js
+++ src/components/SearchTerm/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+
+import { addHistoryAction, deleteHistoryAction } from "../../actions";
 
 function SearchTerm(props) {
+  const dispatch = useDispatch();
+
   return (
-    <div className="d-flex align-items-center mb-2" id={props.index}>
-      <div className="rounded-left bg-light p-2 flex-grow-1">
+    <div className="d-flex mb-2" id={props.index}>
+      <div
+        className="rounded-left bg-white p-2 flex-grow-1"
+        style={{ cursor: "pointer" }}
+        onClick={() => dispatch(addHistoryAction(props.term))}
+      >
         <p>{props.term}</p>
       </div>
-      <div className="rounded-right bg-light p-2">
+      <div
+        className="rounded-right bg-light p-2"
+        style={{ borderLeft: "1px solid #ddd", cursor: "pointer" }}
+        onClick={() => dispatch(deleteHistoryAction(props.index))}
+      >
         <p>
           <i className="fas fa-times"></i>
         </p>
