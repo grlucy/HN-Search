0aa53b437de4bae0292bc820fae0389a299377a1
unknown <53538316+grlucy@users.noreply.github.com>
2020-05-14 10:40:35 -0400

    heroku build

---
+++ build/precache-manifest.cffe24b53f6c9a3f42c47b0f481a7c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7932ca6b9efb589ad3bf38180458dfbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acecbadabef4ab6ca7e5",
+    "url": "/static/css/main.bed01fa4.chunk.css"
+  },
+  {
+    "revision": "b2c8beaadacb32577153",
+    "url": "/static/js/2.dd6435b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dd6435b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acecbadabef4ab6ca7e5",
+    "url": "/static/js/main.5a0da970.chunk.js"
+  },
+  {
+    "revision": "5c264c2ad929c0edb3b5",
+    "url": "/static/js/runtime-main.358b2f98.js"
+  }
+]);
